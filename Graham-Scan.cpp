363a6f5e9d50bc1d2f070b178633ae32126d8db5
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-04-03 01:07:58 +0530

    fix: error causing incorrect pop of duplicate point

--- Graham-Scan.cpp
+++ Graham-Scan.cpp
@@ -17,22 +17,23 @@ int orientation(Point a, Point b, Point c) {
 vector<Point> grahamScan(vector<Point> points) {
     sort(points.begin(), points.end());
     vector<Point> hull;
+
     for (int i = 0; i < 2; i++) {
         int s = hull.size();
         for (Point &p : points) {
-            while (hull.size() >= s + 2 && orientation(hull[hull.size()-2], hull.back(), p) != -1)
+            while (hull.size() >= s + 2 && orientation(hull[hull.size() - 2], hull.back(), p) != -1)
                 hull.pop_back();
             hull.push_back(p);
         }
+        hull.pop_back();
         reverse(points.begin(), points.end());
     }
-    hull.pop_back();
     return hull;
 }
 
 int main() {
     vector<Point> points = {{0, 3}, {2, 2}, {1, 1}, {2, 1}, {3, 0}, {0, 0}, {3, 3}};
-    cout << "Grahams Scan Convex Hull:\n";
+    cout << "Graham's Scan Convex Hull:\n";
     for (auto p : grahamScan(points))
         cout << "(" << p.x << ", " << p.y << ")\n";
     return 0;
