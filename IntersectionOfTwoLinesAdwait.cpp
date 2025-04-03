34efbacb67b2dfece827644562aac163e623e64c
Dharmik Shinde <146475627+iamdmix@users.noreply.github.com>
2025-04-03 12:19:39 +0530

    Create IntersectionOfTwoLinesAdwait.cpp

---
+++ IntersectionOfTwoLinesAdwait.cpp
@@ -0,0 +1,11 @@
+bool doIntersect(Point p1, Point q1, Point p2, Point q2) {
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    return false;
+}
